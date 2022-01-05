6ddb151cb77354b5fc9f91ddef22d59ea6c74570
Haden Santefort <hsantefort12@gmail.com>
2022-01-05 13:27:55 -0600

    Add test code

---
+++ src/components/Questions.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react';
+
+function Questions() {
+    const [error, setError] = useState(null);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [questions, setQuestions] = useState([])
+
+    useEffect(() => {
+        fetch('https://dnd-question-sheet-tracker.herokuapp.com/questions')
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    setIsLoaded(true);
+                    setQuestions(result);
+                },
+                (error) => {
+                    setIsLoaded(true);
+                    setError(error);
+                }
+            )
+    }, [])
+
+    if (error) {
+        return <div>Error: {error.message}</div>
+    } else if (!isLoaded) {
+        return <div>loading...</div>
+    } else {
+        return (
+            <ul>
+                {questions.map(question => (
+                    <li key={question.name}>
+                        {question.name}
+                    </li>
+                ))}
+            </ul>
+        )
+    }
+}
+
+export default Questions;
